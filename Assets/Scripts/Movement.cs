7569215a7529a7e2b5911a258e5226bcf9bd35c3
Anzodroid <anzodroid@gmail.com>
2021-09-18 11:49:07 +1000

    tweening test-box

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -8,16 +8,26 @@ public class Movement : MonoBehaviour
     public GameObject Cube;
     private Vector3 currentPos;
     public Tween tween;
+    private float speed;
+    //private ArrayList path;
+    int path;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        speed = 2.5f;
         currentPos = new Vector3(-11f, 14f, 0f);
+        int i;
+        int path = 1;
+
         //PacStudent.transform.position = currentPos;
         Cube.transform.position = currentPos;
 
         //AddTween(PacStudent.transform, PacStudent.transform.position, new Vector3(0.0f, 13.0f, 0.0f), 0.5f);
-        AddTween(Cube.transform, Cube.transform.position, new Vector3(0.0f, 14.0f, 0.0f), 11.5f);
+        AddTween(Cube.transform, Cube.transform.position, new Vector3(-6.0f, 14.0f, 0.0f), speed);
+
+
 
         Debug.Log("POS" + PacStudent.transform.position);
     }
@@ -25,10 +35,30 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        switch (path)
+        {
+            case 1:
+                AddTween(Cube.transform, Cube.transform.position, new Vector3(-6.0f, 14.0f, 0.0f), speed);
+                break;
+            case 2:
+                AddTween(Cube.transform, Cube.transform.position, new Vector3(-6.0f, 10.0f, 0.0f), speed);
+                break;
+            case 3:
+                AddTween(Cube.transform, Cube.transform.position, new Vector3(-11.0f, 10.0f, 0.0f), speed);
+                break;
+            case 4:
+                AddTween(Cube.transform, Cube.transform.position, new Vector3(-11.0f, 14.0f, 0.0f), speed);
+                path = 0;
+                break;
+
+        }
+
+        
+
         Debug.Log(tween.Target + ":"+ tween.StartPos + ":" + tween.EndPos + ":" + tween.Duration);
         Debug.Log(tween.Target.position + ":" + tween.EndPos + ":" + currentPos);
 
-        if (Vector3.Distance(tween.Target.position, tween.EndPos) > 0.1f)
+        if (Vector3.Distance(tween.Target.position, tween.EndPos) >= 0.01)
         {
         float timeFraction = (Time.time - tween.StartTime) / tween.Duration;
             currentPos = Vector3.Lerp(tween.StartPos, tween.EndPos, timeFraction);
@@ -38,10 +68,12 @@ public class Movement : MonoBehaviour
        else
         {
             tween.Target.position = tween.EndPos;
-            //tween = null;
+            tween = null;
             Debug.Log("NO");
+            path++;
+            //AddTween(Cube.transform, Cube.transform.position, new Vector3(-6.0f, 10.0f, 0.0f), speed);
 
-       }
+        }
 
         //PacStudent.transform.position = currentPos;
         Cube.transform.position = currentPos;
