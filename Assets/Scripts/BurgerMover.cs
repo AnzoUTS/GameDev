0277c68b770f2e9fe0aa5693d1bfe4a0c193f087
Anzodroid <anzodroid@gmail.com>
2021-10-09 10:45:56 +1100

    Credit level complete

--- Assets/Scripts/BurgerMover.cs
+++ Assets/Scripts/BurgerMover.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class BurgerMover : MonoBehaviour
 {
-
     private Tween tween;
     private Vector3 localPos;
     private Vector3 currentPos;
@@ -12,36 +11,29 @@ public class BurgerMover : MonoBehaviour
     private const float centerY = -14.0f;
     private int x;
     private int y;
-    private int axis;
+    private int direction;
     private float destroyX1;
     private float destroyY1;
     private float destroyX2;
     private float destroyY2;
-
-
-
+    private int duration;
 
     void Start()
     {
-
-
+        duration = Random.Range(8, 11); // max Exclusive for int
         destroyX1 = -99;
         destroyX2 = 99;
         destroyY1 = -99;
         destroyY2 = 99;
 
-        Debug.Log("Burger Created");
+      //  Debug.Log("Burger Created");
         x = CherryController.Xvalue;
         y = CherryController.Yvalue;
-        axis = CherryController.Axis;
-
+        direction = CherryController.Axis;
     }
-
-
 
     private void FixedUpdate()
     {
-
         localPos = transform.localPosition;
 
         if (tween != null)
@@ -56,82 +48,61 @@ public class BurgerMover : MonoBehaviour
     {
         x = CherryController.Xvalue;
         y = CherryController.Yvalue;
-        axis = CherryController.Axis;
-
-
+        direction = CherryController.Axis;
 
         if (localPos.x <= destroyX1 || localPos.x >= destroyX2 || localPos.y <= destroyY1 || localPos.y >= destroyY2)
         {
-            Debug.Log("Burger Destroyed");
+          //  Debug.Log("Burger Destroyed");
             Destroy(transform.gameObject);
-
-
         }
 
 
-        switch (axis)
+        switch (direction)
         {
             case 1:
                 {
-
-                    /*                  2 -14 -30 40
-                                        destroyX1 = -17;
-                                        destroyX2 = 44;
-                                        destroyY1 = -30;
-                                        destroyY2 = 99;*/
-
                     // --> down
-                    destroyX1 = -17;
-                    destroyX2 = 44;
-                    destroyY1 = -30; 
-                    destroyY2 = 99;// main
-                    AddTween(transform, transform.localPosition, new Vector3((-x) + centerX, -33, 0), 10);
-    
-
+                    destroyX1 = -34;
+                    destroyX2 = 40;
+                    destroyY1 = -30; // main target
+                    destroyY2 = 99;
+                    AddTween(transform, transform.localPosition, new Vector3((-x) + centerX, -33, 0), duration);
                     break;
                 }
 
             case 2:
                 {
                     // --> left
-                    destroyX1 = -13; // main
-                    destroyX2 = 44;
-                    destroyY1 = -30;
-                    destroyY2 = 2;
-                    AddTween(transform, transform.localPosition, new Vector3(-15, (-y) + centerY, 0), 8);
-   
-
+                    destroyX1 = -14; // main target
+                    destroyX2 = 99;   
+                    destroyY1 = -35;
+                    destroyY2 = 7;
+                    AddTween(transform, transform.localPosition, new Vector3(-15, (-y) + centerY, 0), duration);
                     break;
                 }
             case 3:
                 {
                     // --> up
-                    destroyX1 = -17;
-                    destroyX2 = 44;
-                    destroyY1 = -99; // main
-                    destroyY2 = 2; 
+                    destroyX1 = -34;
+                    destroyX2 = 40;
+                    destroyY1 = -99; 
+                    destroyY2 = 2;  // main target
                     AddTween(transform, transform.localPosition, new Vector3((x) + centerX, 5, 0), 10);
-                 
                     break;
                 }
 
             case 4:
                 {
                     // --> right
-                    destroyX1 = -99;// main
-                    destroyX2 = 40; 
-                    destroyY1 = -30;
-                    destroyY2 = 2;
+                    destroyX1 = -99;
+                    destroyX2 = 40;  // main target
+                    destroyY1 = -35; 
+                    destroyY2 = 7;
                     AddTween(transform, transform.localPosition, new Vector3(45, (y) + centerY, 0), 8);
-           
-
                     break;
                 }
-
         }
-
-
-        
+  
     }
 
     public void AddTween(Transform targetObject, Vector3 startPos, Vector3 endpos, float duration)
@@ -139,8 +110,7 @@ public class BurgerMover : MonoBehaviour
         
         if (tween == null)
         {
-            //Debug.Log("x Target " + endpos.x + "|| y Target " + endpos.y);
-            Debug.Log("x1 " + destroyX1 + "|| x2 " + destroyX2 + "|| y1 " + destroyY1 + "|| y2 " + destroyY2 + "x Target " + endpos.x + "|| y Target " + endpos.y);
+           // Debug.Log("x1 " + destroyX1 + "|| x2 " + destroyX2 + "|| y1 " + destroyY1 + "|| y2 " + destroyY2 + "x Target " + endpos.x + "|| y Target " + endpos.y);
             tween = new Tween(targetObject, startPos, endpos, Time.time, duration);
         }
     }
--- Assets/Scripts/CherryController.cs
+++ Assets/Scripts/CherryController.cs
@@ -8,27 +8,12 @@ public class CherryController : MonoBehaviour
     public GameObject cherry;
     public static int x;
     public static int y;
-    private static int axis;
+    private static int direction;
     private const float centerX = 13.5f;
     private const float centerY = -14.0f;
 
-
-
-    void Start()
-    {
-/*        x = Random.Range(-30, 30);
-        y = Random.Range(-30, 30);
-        axis = Random.Range(1, 5);
-
-*/
-
-    }
-
-
-
     void Update()
     {
-
         if (!isBurger)
         {
             isBurger = true;
@@ -36,69 +21,46 @@ public class CherryController : MonoBehaviour
         }
     }
 
-
-
-
     private IEnumerator CherryBurger()
     {
-     
-        x = Random.Range(-30, 30);
-        y = Random.Range(-30, 30);
-        axis = Random.Range(1, 5); // check random max for int (5)
+        x = Random.Range(-20, 20);
+        y = Random.Range(-20, 20);
+        direction = Random.Range(1, 5); // max Exclusive for int
 
-        switch (axis)
+        switch (direction)
         {
             case 1: 
                 {
                     Instantiate(cherry, new Vector3(x + centerX, 5, 0), Quaternion.Euler(0, 0, 0));
-                    Debug.Log("x Start " + (x + centerX) + "|| y Start " + 5 + "|| axis :" + axis);
+                  //  Debug.Log("x Start " + (x + centerX) + "|| y Start " + 5 + "|| axis :" + axis);
                     break;
                 }
 
             case 2:
                 {
                     Instantiate(cherry, new Vector3(40, y + centerY, 0), Quaternion.Euler(0, 0, 0));
-                    Debug.Log("x Start " + 40 + "|| y Start " + (y + centerY) + "|| axis :" + axis);
+                 //   Debug.Log("x Start " + 40 + "|| y Start " + (y + centerY) + "|| axis :" + axis);
                     break;
                 }
 
             case 3:
                 {
                     Instantiate(cherry, new Vector3(-x + centerX, -33, 0), Quaternion.Euler(0, 0, 0));
-                    Debug.Log("x Start " + (-x + centerX) + "|| y Start " + -33 + "|| axis :" + axis);
+                //    Debug.Log("x Start " + (-x + centerX) + "|| y Start " + -33 + "|| axis :" + axis);
                     break;
                 }
 
             case 4:
                 {
                     Instantiate(cherry, new Vector3(-15, -y + centerY, 0), Quaternion.Euler(0, 0, 0));
-                    Debug.Log("x Start " + -15 + "|| y Start " +  (-y + centerY) + "|| axis :" + axis);
+                //    Debug.Log("x Start " + -15 + "|| y Start " +  (-y + centerY) + "|| axis :" + axis);
                     break;
                 }
-
-
-                //Debug.Log("x Start " + x + "|| y Start " + y + "|| axis :" + axis);
-
-
-
-
-
-
         }
 
-
-        //Debug.Log("x Start "+ x + "|| y Start " + y + "|| axis :" + axis);
-
-
-   
-
-
-
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(10f);
         isBurger = false;
-
     }
-
 
     public static int Xvalue
     {
@@ -116,16 +78,13 @@ public class CherryController : MonoBehaviour
         }
     }
 
-
     public static int Axis
     {
         get
         {
-            return axis;
+            return direction;
         }
     }
-
-
 
 }
 
