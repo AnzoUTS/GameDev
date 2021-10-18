95183f996560c85250712f4fd7aa59b28830b5ce
Anzodroid <anzodroid@gmail.com>
2021-10-18 20:08:14 +1100

    more AI functionality added

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    // the code below is from 80% section and no longer used
     private Animator anim;
     private bool isAlive; 
 
     void Start()
     {
         anim = GetComponent<Animator>();
-/*        anim.SetBool("up", false);
+/*      anim.SetBool("up", false);
         anim.SetBool("down", false);
         anim.SetBool("left", true);
         anim.SetBool("right", false);*/
@@ -20,8 +21,9 @@ public class Enemy : MonoBehaviour
     void Update()
     {
 
-        if (GameManagement.Scared == true && isAlive == true)
-        {
+        //if (GameManagement.Scared == true)
+           if (GameManagement.Scared == true && isAlive == true)
+            {
             anim.SetBool("up", false);
             anim.SetBool("down", false);
             anim.SetBool("left", false);
@@ -32,34 +34,33 @@ public class Enemy : MonoBehaviour
        // Debug.Log("GM Scared" + GameManagement.Scared + " || Animations : anim UP :" + anim.GetBool("up") + " anim RIGHT :" + anim.GetBool("right") + " anim DOWN :" + anim.GetBool("down") + " anim LEFT :" + anim.GetBool("left") + " || scared " + anim.GetBool("isScared") + " is recovery "+anim.GetBool("isRecovery") + " is DEad" + anim.GetBool("isDead"));
     }
 
-    private void OnTriggerEnter(Collider trigger)
-    {
-        if (GameManagement.Scared == true)
+    /*    private void OnTriggerEnter(Collider trigger) Removed from 80% Section
         {
-            isAlive = false;
+            if (GameManagement.Scared == true)
+            {
+                isAlive = false;
+                anim.SetBool("isScared", false);
+                anim.SetBool("isRecovery", false);
+                anim.SetBool("isDead", true);
+                AudioController.GhostDead = true;
+                GameManagement.Score += 300;
+              //  StartCoroutine(EnemyDead()); // Removed from 80% Section
+            }
+        }
+
+        IEnumerator EnemyDead() // only used for 80% Section 
+        {
+            CancelInvoke("Recovery");
+            CancelInvoke("NormalState");
+
             anim.SetBool("isScared", false);
             anim.SetBool("isRecovery", false);
-            anim.SetBool("isDead", true);
-            AudioController.GhostDead = true;
-            GameManagement.Score += 300;
-            StartCoroutine(EnemyDead());
-        }
-    }
-
-    IEnumerator EnemyDead()
-    {
-        CancelInvoke("Recovery");
-        CancelInvoke("NormalState");
-
-        anim.SetBool("isScared", false);
-        anim.SetBool("isRecovery", false);
-        yield return new WaitForSeconds(5);
-      
-        AudioController.GhostDead = false;
-        isAlive = true;
-        anim.SetBool("isDead", false);
-        anim.SetBool("up", true);
-    }
+            yield return new WaitForSeconds(5);
+            AudioController.GhostDead = false;
+            isAlive = true;
+            anim.SetBool("isDead", false);
+            anim.SetBool("up", true);
+        }*/
 
     void Recovery()
     {
--- Assets/Scripts/GhostController.cs
+++ Assets/Scripts/GhostController.cs
@@ -16,6 +16,7 @@ public class GhostController : MonoBehaviour
     private Vector3 NormPos;
     private Vector3 localPos;
     public List<Vector3> Walkable;
+    public List<Vector3> GhostArea;
     private Tween tween;
     private Animator anim;
     private GameObject[] gameObjects;
@@ -35,10 +36,12 @@ public class GhostController : MonoBehaviour
     private Vector3 direction;
     private Vector3 previousDirection;
     private Vector3 lastDirection;
+    private Vector3 startingPos;
+    private bool isAlive;
 
     void Start()
     {
-        speed = 2.5f;
+        speed = 2f;
         boxCollider = GetComponent<BoxCollider>();
         anim = GetComponent<Animator>();
         ghostLocation = transform.position;
@@ -49,10 +52,20 @@ public class GhostController : MonoBehaviour
         foreach (GameObject item in gameObjects)
         {
             Walkable.Add(item.transform.position);
+            if (item.name.Contains("GhostArea"))
+                GhostArea.Add(item.transform.position);
         }
+        if (enemyName == "OrcC")
+        {
+            startingPos = new Vector3(14f, -14f, 0f);
+           
+        }
+        isAlive = true;
 
-        NormPos = new Vector3(0, 1, 0);
-        tween = null;
+
+        Debug.Log("GhostArea " + GhostArea.Count);
+        /*    NormPos = new Vector3(0, 1, 0);
+            tween = null;*/
     }
 
     void Update()
@@ -67,9 +80,7 @@ public class GhostController : MonoBehaviour
             transform.position = currentPos;
         }
 
-        if (currentInput == 0 && enemyName == "OrcC")
-        {
-        }
+
 
          movement = speed * Time.deltaTime;
 
@@ -107,52 +118,83 @@ public class GhostController : MonoBehaviour
 
         if  (tween == null)
         {
-            if (canMove == true)
+            if (canMove == true && isAlive)
             {
                 GhostBrain();
+            } else if (!isAlive)
+            {
+                AddTween(transform, localPos, startingPos, 10);
             }
+
+            
+
+
+ 
         }
-    }
-
-    public void Direction(Vector3 newDirection)
-    {
-        int x = (int)Math.Round(lastPosition.x);
-        int y = (int)Math.Round(lastPosition.y);
-        float z = lastPosition.z;
-
-        if (newDirection == new Vector3(x-1, y + 1, z))
+        if (!GameManagement.Scared)
         {
+            if (NormPos == new Vector3(-1, 0, 0))
+            {
                 anim.SetBool("left", true);
                 anim.SetBool("down", false);
                 anim.SetBool("up", false);
                 anim.SetBool("right", false);
-        }
-        else if (newDirection == new Vector3(x+1, y, z))
-        {
+            }
+            if (NormPos == new Vector3(1, 0, 0))
+            {
                 anim.SetBool("right", true);
                 anim.SetBool("down", false);
                 anim.SetBool("left", false);
                 anim.SetBool("up", false);
-        }
-        else if (newDirection == new Vector3(x, y + 1, z))
-    {
+            }
+            if (NormPos == new Vector3(0, 1, 0))
+            {
                 anim.SetBool("up", true);
                 anim.SetBool("down", false);
                 anim.SetBool("left", false);
                 anim.SetBool("right", false);
-        }
-        else if(newDirection == new Vector3(x, y - 1, z))
-    {
+            }
+            if (NormPos == new Vector3(0, -1, 0))
+            {
                 anim.SetBool("down", true);
                 anim.SetBool("up", false);
                 anim.SetBool("left", false);
                 anim.SetBool("right", false);
+            }
         }
-        else
+        else if (isAlive)
         {
-            Debug.Log("Ghost is confused : direction provided" + newDirection);
+            anim.SetBool("up", false);
+            anim.SetBool("down", false);
+            anim.SetBool("left", false);
+            anim.SetBool("right", false);
+            anim.SetBool("isScared", true);
+            Invoke("Recovery", 7f);
+
         }
+        else if (!isAlive)
+        {
+            anim.SetBool("isScared", false);
+            anim.SetBool("isRecovery", false);
+            anim.SetBool("isDead", true);
+        }
+        /*        if (GameManagement.Scared == true && isAlive == true)
+                {
+                    anim.SetBool("up", false);
+                    anim.SetBool("down", false);
+                    anim.SetBool("left", false);
+                    anim.SetBool("right", false);
+                    anim.SetBool("isScared", true);
+                    Invoke("Recovery", 7f);
+                }*/
+
+
+
     }
+
+
+
+
 
     private void GhostBrain()
     {
@@ -174,13 +216,13 @@ public class GhostController : MonoBehaviour
 
         if (lastDirection.y == 1 && lastDirection.x == 0) // up
         {
-            if (Walkable.Contains(up)) // up
+            if (Walkable.Contains(up) && !GhostArea.Contains(up)) // up
                 ghostOptions.Add(up);
 
-            if (Walkable.Contains(left)) // left
+            if (Walkable.Contains(left) && !GhostArea.Contains(left)) // left
                 ghostOptions.Add(left);
 
-            if (Walkable.Contains(right)) // right
+            if (Walkable.Contains(right) && !GhostArea.Contains(right)) // right
                 ghostOptions.Add(right);
 
             moveGhost();
@@ -189,13 +231,13 @@ public class GhostController : MonoBehaviour
 
         else if (lastDirection.y == -1 && lastDirection.x == 0) // down
         {
-            if (Walkable.Contains(down)) // down
+            if (Walkable.Contains(down) && !GhostArea.Contains(down)) // down
                 ghostOptions.Add(down);
 
-            if (Walkable.Contains(left)) // left
+            if (Walkable.Contains(left) && !GhostArea.Contains(left)) // left
                 ghostOptions.Add(left);
 
-            if (Walkable.Contains(right)) // right
+            if (Walkable.Contains(right) && !GhostArea.Contains(right)) // right
                 ghostOptions.Add(right);
 
             moveGhost();
@@ -204,13 +246,13 @@ public class GhostController : MonoBehaviour
 
         else if (lastDirection.x == -1 && lastDirection.y == 0) // left
         {
-            if (Walkable.Contains(up)) // up
+            if (Walkable.Contains(up) && !GhostArea.Contains(up)) // up
                 ghostOptions.Add(up);
 
-            if (Walkable.Contains(down)) // down
+            if (Walkable.Contains(down) && !GhostArea.Contains(down)) // down
                 ghostOptions.Add(down);
 
-            if (Walkable.Contains(left)) // left
+            if (Walkable.Contains(left) && !GhostArea.Contains(left)) // left
                 ghostOptions.Add(left);
 
             moveGhost();
@@ -218,13 +260,13 @@ public class GhostController : MonoBehaviour
 
         else if (lastDirection.x == 1 && lastDirection.y == 0) // right
         {
-            if (Walkable.Contains(down)) // down
+            if (Walkable.Contains(down) && !GhostArea.Contains(down)) // down
                 ghostOptions.Add(down);
 
-            if (Walkable.Contains(right)) // right
+            if (Walkable.Contains(right) && !GhostArea.Contains(right)) // right
                 ghostOptions.Add(right);
 
-            if (Walkable.Contains(up)) // up
+            if (Walkable.Contains(up) && !GhostArea.Contains(up)) // up
                 ghostOptions.Add(up);
 
             moveGhost();
@@ -232,13 +274,14 @@ public class GhostController : MonoBehaviour
 
         else
         {
-            Debug.Log("confused!!!!!!!!!!!!!!!!!!!! ################################################################" + previousDirection);
 
-/*            Debug.Log("Transform " + transform + "localPos " + localPos + "direction " + direction + "(direction-lastDirection)" + (direction - lastDirection) + "(direction + lastDirection) " + (direction + lastDirection));
 
-            AddTween(transform, new Vector3(x, y, z), (direction + lastDirection), duration);
-          */
+       
         }
+
+  
+
+
     }
 
     public void AddTween(Transform targetObject, Vector3 startPos, Vector3 endpos, float duration)
@@ -255,20 +298,88 @@ public class GhostController : MonoBehaviour
         int y = (int)Math.Round(localPos.y);
         float z = localPos.z;
 
-        ghostThought = UnityEngine.Random.Range(0, ghostOptions.Count);
-        Debug.Log("Normal pos " + NormPos + "Valid options " + ghostOptions.Count + " lastPosition" + lastPosition + " localpos" + localPos + " ghostThought " + ghostThought);
-
-        foreach (Vector3 option in ghostOptions)
+        if (ghostOptions.Count == 0)
         {
-            Debug.Log("ghostArrayOptions " + option);
+            Debug.Log("Ghost has no other option");
+
+            if (Walkable.Contains(up) && !GhostArea.Contains(up)) // up
+                ghostOptions.Add(up);
+
+            if (Walkable.Contains(left) && !GhostArea.Contains(left)) // left
+                ghostOptions.Add(left);
+
+            if (Walkable.Contains(right) && !GhostArea.Contains(right)) // right
+                ghostOptions.Add(right);
+
+            if (Walkable.Contains(down) && !GhostArea.Contains(down)) // down
+                ghostOptions.Add(down);
         }
 
-        direction = ghostOptions[ghostThought];
+            ghostThought = UnityEngine.Random.Range(0, ghostOptions.Count);
+            Debug.Log("Normal pos " + NormPos + "Valid options " + ghostOptions.Count + " lastPosition" + lastPosition + " localpos" + localPos + " ghostThought " + ghostThought);
 
-        lastDirection = direction - new Vector3(x, y, z);
-        Debug.Log("last direction : " + lastDirection);
+            foreach (Vector3 option in ghostOptions)
+            {
+                Debug.Log("ghostArrayOptions " + option);
+            }
+
+            direction = ghostOptions[ghostThought];
+
+            lastDirection = direction - new Vector3(x, y, z);
+            Debug.Log("last direction : " + lastDirection);
+    
 
         AddTween(transform, new Vector3(x, y, z), direction, duration);
     }
+
+
+
+
+    private void OnTriggerEnter(Collider trigger)
+    {
+        if (GameManagement.Scared == true)
+        {
+            Debug.Log(enemyName + "is Dead");
+            isAlive = false;
+     /*       anim.SetBool("isScared", false);
+            anim.SetBool("isRecovery", false);
+            anim.SetBool("isDead", true);*/
+            AudioController.GhostDead = true;
+            GameManagement.Score += 300;
+            StartCoroutine(EnemyDead()); // Removed from 80% Section
+            
+        }
+    }
+
+
+    void Recovery()
+    {
+        anim.SetBool("isRecovery", true);
+        anim.SetBool("isScared", false);
+        Invoke("NormalState", 3f);
+    }
+
+    void NormalState()
+    {
+        anim.SetBool("isRecovery", false);
+        anim.SetBool("isDead", false);
+        anim.SetBool("up", true);
+    }
+
+
+    IEnumerator EnemyDead() // only used for 80% Section 
+    {
+        CancelInvoke("Recovery");
+        CancelInvoke("NormalState");
+
+        anim.SetBool("isScared", false);
+        anim.SetBool("isRecovery", false);
+        yield return new WaitForSeconds(15);
+        AudioController.GhostDead = false;
+        isAlive = true;
+        anim.SetBool("isDead", false);
+        anim.SetBool("up", true);
+    }
+
 
 }
